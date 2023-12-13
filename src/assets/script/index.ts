de78a3392d300838f78aac1471b007034edcdb97
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-13 13:16:50 +0200

    add foto pizza and fun chenge foto on main page

--- src/assets/script/index.ts
+++ src/assets/script/index.ts
@@ -1,2 +1,17 @@
-const value: string = 'Hello World';
-console.log(value);
+let counter: number = 5;
+const imgBlock: HTMLElement= document.querySelector(".pizza__img")!;
+const img: HTMLImageElement = document.createElement('img');
+img.src = `./assets/image/pizza-${counter}.svg`;
+img.alt = 'pizza';
+
+function chengeImg(value: string): void {
+    img.src = '';
+    if (value === 'left' && counter > 1) {
+        counter--;
+    } else if (value === 'right' && counter < 9) {
+        counter++;
+    }
+    img.src = `./assets/image/pizza-${counter}.svg`;
+}
+
+imgBlock.append(img);
